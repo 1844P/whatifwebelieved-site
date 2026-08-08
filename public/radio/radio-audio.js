3eb1de5598387bf7a96273fc2f5e1a49cb1f3f8f
1844P <147441805+1844P@users.noreply.github.com>
2026-08-08 07:05:57 -0400

    Fix missing buildNoise function

--- public/radio/radio-audio.js
+++ public/radio/radio-audio.js
@@ -78,6 +78,13 @@ function ensure() {
         hymnGain.connect(volGain);
     }
 
+    function buildNoise(seconds) {
+        const buffer = ctx.createBuffer(1, ctx.sampleRate * seconds, ctx.sampleRate);
+        const data = buffer.getChannelData(0);
+        for (let i = 0; i < data.length; i++) data[i] = Math.random() * 2 - 1;
+        return buffer;
+    }
+
     /* ---------- YouTube Player API ---------- */
 
     function loadYouTubeAPI() {
